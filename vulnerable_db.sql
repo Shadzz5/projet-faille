3803c3953cea31cd142e800d20891f5daa840eb9
kevin <quiercelinkevin@gmail.com>
2025-03-07 13:32:07 +0100

    add new field email

--- vulnerable_db.sql
+++ vulnerable_db.sql
@@ -37,7 +37,8 @@ SET time_zone = "+00:00";
 CREATE TABLE `users` (
   `id` int NOT NULL,
   `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
